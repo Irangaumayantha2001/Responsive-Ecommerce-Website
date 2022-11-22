759ca0dd10e4686b1744bfde71f8048cbc8097f8
Irangaumayantha2001 <iamiranga9@gmail.com>
2022-11-22 15:32:06 +0530

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,16 @@ const showMenu = (toggleId, navId) => {
   }
 };
 
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu');
+
+const navLink=document.querySelectorAll('.nav_link')
+
+function linkAction(){
+  navLink.forEach(n => n.classList.remove('active'))
+  this.classList.add('active')
+
+  const navMenu =document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
